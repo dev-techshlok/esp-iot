0d0972ab0cea42c130824111bb2a6e3dee702c6c
dev-techshlok <54532217+dev-techshlok@users.noreply.github.com>
2023-01-06 23:04:30 +0530

    Shows internal temperature of ESP32

---
+++ get_temprature.ino
@@ -0,0 +1,30 @@
+
+ #ifdef __cplusplus
+  extern "C" {
+ #endif
+
+  uint8_t temprature_sens_read();
+
+#ifdef __cplusplus
+}
+#endif
+
+uint8_t temprature_sens_read();
+//====================================================
+//         Setup
+//====================================================
+void setup() {
+  Serial.begin(115200);
+}
+
+//====================================================
+//         Loop
+//====================================================
+void loop() {
+  Serial.print("Temperature: ");
+  
+  // Convert raw temperature in F to Celsius degrees
+  Serial.print((temprature_sens_read() - 32) / 1.8);
+  Serial.println(" C");
+  delay(1000);
+}
